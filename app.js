078f0976f105f72cc92cd7923e94916c6e278c9c
MillieGrey <garethgreyscale@gmail.com>
2021-07-15 22:48:21 -0500

    inported elements, svg. tried some things

---
+++ app.js
@@ -0,0 +1,21 @@
+import anime from '/node_modules/animejs/lib/anime.es.js';
+
+
+var elements = document.querySelectorAll('.dom-node-demo .el');
+
+anime({
+    targets: '.fuc',
+    translateX: 0,
+    translatey: -200,
+    rotate: '1turn',
+    duration: 80000
+  });
+
+anime({
+    targets:'.svg .lines path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    delay: function(el, i) { return i * 250 },
+    direction: 'forwards',
+})
