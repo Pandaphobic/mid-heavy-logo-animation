40df06b9661149d1d9c28955dd4ef02b1dd5f45f
Pandaphobic <c.steffes@live.ca>
2021-07-24 13:37:14 -0500

    first commit

--- app.js
+++ app.js
@@ -1,98 +1,63 @@
-import anime from '/node_modules/animejs/lib/anime.es.js';
+/*       --- Mid Heavy Timeline Animation ---       */
+var basicTimeline = anime.timeline();
+basicTimeline.reverse();
 
-
-var elements = document.querySelectorAll('.dom-node-demo .el');
-
-anime({
-    targets: '.fuc',
-    translateX: 0,
-    translatey: -200,
-    rotate: '1turn',
-    duration: 80000
-});
-
-var backPath1;
-var backPath2;
-var backPath3;
-var backPath4;
-var backPath;
-
-// window.onload = function (){
-//   var c = document.getElementById("mhlogo").contentDocument;
-//   backPath1 = c.getElementById("backPath1");
-//   backPath2 = c.getElementById("backPath2");
-//   backPath3 = c.getElementById("backPath3");
-//   backPath4 = c.getElementById("backPath4");
-//   backPath = c.querySelectorAll("backPath");
-//   console.log(backPath)
-//     anime({
-//       targets: backPath1,
-//       translateY: 250,
-//       translateX: -35,
-//       direction: 'reverse',
-//       easing: 'easeInOutSine',
-//       duration: 2000,
-//       delay: 1000,
-//     });
-//     anime({
-//       targets: backPath2,
-//       translateY: 250,
-//       translateX: -35,
-//       direction: 'reverse',
-//       easing: 'easeInOutSine',
-//       duration: 2000,
-//       delay: 4000,
-//     });
-//     anime({
-//       targets: backPath3,
-//       translateY: 250,
-//       translateX: -35,
-//       direction: 'reverse',
-//       easing: 'easeInOutSine',
-//       duration: 2000,
-//       delay: 6000,
-//     });
-//     anime({
-//       targets: backPath4,
-//       translateY: 250,
-//       translateX: -35,
-//       direction: 'reverse',
-//       easing: 'easeInOutSine',
-//       duration: 2000,
-//       delay: 8000,
-//     });
-// }
-window.onload = function (){
-anime({
-  targets: '.backPath',
-  translateY: 250,
-  translateX: -37,
-  direction: 'reverse',
-  easing: 'easeInOutSine',
-  duration: 1000,
-  delay: anime.stagger(2000, {direction: 'reverse'})
-  
-});
-
-anime({
-  targets: '.frontPath',
-  translateY: -250,
-  translateX: -37,
-  direction: 'reverse',
-  easing: 'easeInOutSine',
-  duration: 1000,
-  delay: anime.stagger(2000, {direction: 'reverse'}),
-});
-
-anime({
-  targets: '.hPath',
-  translateX: -4000,
-  direction: 'reverse',
-  easing: 'easeInOutSine',
-  duration: 1000,
-  delay: 8000,
-});
+function title() {
+  document.getElementById("title").style.display = "block";
 }
-//hey hon, figure out a way to itterate this so that you dont have to write it out a bunch of times for each element.
-//use animejs staggering for moving all the things, might wanna pull the animejs func out of the onload?
-//gotta give all your svg paths ids to target them by.
+
+function H() {
+  // Make the line visible
+  document.getElementById("hPath").style.display = "block";
+  return {
+    targets: "#hPath",
+    translateX: 100,
+    direction: "reverse",
+    easing: "easeInOutExpo",
+    duration: 150,
+    delay: 100,
+    complete: title,
+  };
+}
+
+function Back(backTarget) {
+  return {
+    targets: backTarget,
+    translateY: 212,
+    translateX: -35,
+    direction: "reverse",
+    easing: "linear",
+    duration: 110,
+  };
+}
+
+function Front(frontTarget) {
+  return {
+    targets: frontTarget,
+    translateY: -212,
+    translateX: -35,
+    direction: "reverse",
+    easing: "linear",
+    duration: 110,
+  };
+}
+
+// This runs from the bottom up
+basicTimeline
+  // **End Here**
+  .add(Front(`#frontPath5`))
+  .add(Back(`#backPath5`))
+  .add(H())
+
+  .add(Front(`#frontPath4`))
+  .add(Back(`#backPath4`))
+
+  .add(Front(`#frontPath3`))
+  .add(Back(`#backPath3`))
+
+  .add(Front(`#frontPath2`))
+  .add(Back(`#backPath2`))
+
+  .add(Front(`#frontPath1`))
+  .add(Back(`#backPath1`));
+// **Start Here**
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Mid Heavy Timeline Animation
+
+I decided to use the AnimeJS Timeline as it's easier
+to set the order of events. It also waits for the event
+to complete before firing the next.
+
+The timeline wants {animation objects} for each event.
+Because each event will have a different target, I used
+a function for each type of object. (Front, Back, H)
+
+Each of these functions works the same: it accepts the
+target ID and then builds out an object with that ID.
+Once the object is built, add() can then draw it.
+
+You can think of each add() as:
+`anime(animation-object)`
+
+animation-object example:
+`{ targets: backTarget, translateY: 212, translateX: -35, direction: "reverse", easing: "linear", duration: 110, }`
+
+But instead of explicitly declaring each and every object,
+we run a function to create that object imediately before
+we need to use it.
+
+## Result
